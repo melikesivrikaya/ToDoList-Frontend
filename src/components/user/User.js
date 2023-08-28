68af444737317a741ffb5c35571dc8f23de4a818
Melike <melike.svrkya@gmail.com>
2023-08-28 17:56:46 +0300

    user page update

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,9 +1,93 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { useValues } from "../../context/Context";
+import axios from "axios";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  CardText,
+  Container,
+  Row,
+  Col,
+  ListGroupItem,
+  ListGroupItemHeading,
+  ListGroupItemText,
+  ListGroup
+} from "reactstrap";
+import {Link} from "react-router-dom"
 export default function User() {
+  const { currentUserId } = useValues();
+  const [user, setUser] = useState();
+  useEffect(() => {
+    axios(`http://localhost:2020/users/user?id=${currentUserId}`).then((res) =>
+      setUser(res.data)
+    );
+  }, []);
   return (
     <div>
-      
+      <Container>
+        <Card inverse>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/900/270?grayscale"
+            style={{
+              height: 270,
+            }}
+            width="100%"
+          />
+          <CardImgOverlay>
+            <CardText>
+              <small className="text-muted">{user?.name}</small>
+            </CardText>
+          </CardImgOverlay>
+        </Card>
+        <Row>
+          <Col>
+            <h4>Kişisel Bilgiler</h4>
+            <ListGroup>
+              <ListGroupItem>
+                <ListGroupItemHeading>Ad Soyad</ListGroupItemHeading>
+                <ListGroupItemText>{user?.name}</ListGroupItemText>
+              </ListGroupItem>
+              <ListGroupItem>
+                <ListGroupItemHeading>Adres</ListGroupItemHeading>
+                <ListGroupItemText>{user?.address}</ListGroupItemText>
+              </ListGroupItem>
+              <ListGroupItem>
+                <ListGroupItemHeading>Yaş</ListGroupItemHeading>
+                <ListGroupItemText>34</ListGroupItemText>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col>
+            <h4>Arkadaşlar</h4>
+            <ListGroup>
+              <ListGroupItemText>
+                {user?.friendList.map((f) => ( f.friendState === "SUCCESS" ?
+                  <ListGroupItem>
+                    <ListGroupItemHeading>{f.name}</ListGroupItemHeading>{" "}
+                  </ListGroupItem> : <div></div>
+                ))}
+              </ListGroupItemText>
+            </ListGroup>
+          </Col>
+          <Col>
+            <h4>İstekler</h4>
+            <ListGroup>
+              <ListGroupItemText>
+                {user?.friendList.map((f) => ( 
+                  f.friendState ==="WAIT" ?
+                  <ListGroupItem>
+                    <ListGroupItemHeading>{f.name}</ListGroupItemHeading>{" "}
+                    <Link>Kabul Et</Link>
+                  </ListGroupItem> : <div></div>
+                ))}
+              </ListGroupItemText>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
--- src/components/user/UserList.js
+++ src/components/user/UserList.js
@@ -1,13 +1,24 @@
 import React from "react";
 import {useValues } from "../../context/Context";
+import { Button, Col, Container, ListGroup, ListGroupItem } from "reactstrap";
+import {Link} from 'react-router-dom'
 export default function UserList() {
   const { usersValues} = useValues();
+  const {currentUserId} = useValues();
   return (
     <div>
       <h4>User List</h4>
-    {usersValues.users.map((u) => (
-          <a>{u.name} </a>
-     ))}
+      <Container>
+       <Col xs="3">
+       <ListGroup>
+           {usersValues.users?.map((u) => ( u.id != currentUserId ? 
+          <ListGroupItem color="success"  >{u.name} 
+          <Link><Button color="light">Follow</Button></Link>
+          </ListGroupItem> : <div></div>
+        ))}
+        </ListGroup></Col>
+       
+      </Container>
     </div>
   );
 }
